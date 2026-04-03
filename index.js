172eef2951457c852532fd3a628b63c26efba013
Mathew101Q <mathewgabrielmolato00@gmail.com>
2026-04-03 20:19:46 +0800

    Update bot connection parameters and messages

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const mineflayer = require('mineflayer');
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'JMCORESMP.play.hosting', // Example: jmcoresmp.aternos.me
-        port: 25565,           // Usually 25565
-        username: 'AFK_Bot',   // The name of your bot
-        version: '1.20.1'      // Change to your server's version
+        host: 'YOUR_SERVER_IP', // Example: jmcoresmp.aternos.me
+        port: 25565,           // Your server port
+        username: 'AFK_Bot',   // The bot's name
+        version: '1.20.1'      // Your server version
     });
 
     bot.on('login', () => {
-        console.log('Bot has logged in!');
+        console.log('Bot is online!');
     });
 
     // Anti-AFK: Makes the bot jump every 30 seconds
@@ -20,8 +20,8 @@ function createBot() {
 
     // Auto-reconnect if kicked
     bot.on('end', () => {
-        console.log('Disconnected. Reconnecting in 5 seconds...');
-        setTimeout(createBot, 5000);
+        console.log('Bot disconnected. Reconnecting in 10 seconds...');
+        setTimeout(createBot, 10000);
     });
 }
 
